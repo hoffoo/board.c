352878e829682b02fef23400be3d8ee20ba888fc
Marin <marin@stayke.net>
2015-03-20 21:16:35 -0700

    add testing framework and pawn, knight, and rook checks

--- board.c
+++ board.c
@@ -11,11 +11,18 @@
 #define QUEEN 5
 #define KING 6
 
+#define START_PAWN (i > 7 && i < 16) || (i > 47 && i < 56)
+#define START_ROOK i == 0 || i == 7 || i == 56 || i == 63
+#define START_KNIGHT i == 1 || i == 6 || i == 57 || i == 62
+#define START_BISHOP i == 2 || i == 5 || i == 58 || i == 61
+#define START_QUEEN i == 3 || i == 59
+#define START_KING i == 4 || i == 60
+
 #define COLOR_RED "\x1B[34m"
 #define COLOR_BLUE "\x1B[31m"
 #define COLOR_RESET "\x1B[0m"
 
-#define DEBUG
+#define DEBUG_off
 
 
 char piece_to_char(char p) {
@@ -36,6 +43,22 @@ char piece_to_char(char p) {
     }
 }
 
+char max(char a, char b) {
+
+    if (a > b) {
+        return a;
+    }
+    return b;
+}
+
+char min(char a, char b) {
+
+    if (a < b) {
+        return a;
+    }
+    return b;
+}
+
 
 void print_board(char* board) {
 
@@ -45,21 +68,17 @@ void print_board(char* board) {
             printf("\n");
         }
 
-        if (board[i] == 0) {
+        if (!board[i]) {
             printf("o");
             continue;
         }
 
         char black = board[i] << 7 | 0;
         if (black) {
-            printf(COLOR_RED);
+            printf("%s%c%s", COLOR_RED, piece_to_char(board[i]), COLOR_RESET);
         } else {
-            printf(COLOR_BLUE);
+            printf("%s%c%s", COLOR_BLUE, piece_to_char(board[i]), COLOR_RESET);
         }
-
-        printf("%c", piece_to_char(board[i]));
-
-        printf(COLOR_RESET);
     }
 }
 
@@ -72,39 +91,39 @@ void init_board(char* board) {
             color = 1;
         }
 
-        if ((i > 7 && i < 16) || (i > 47 && i < 56)) {
+        if (START_PAWN) {
             board[i] = PAWN << 1 | color;
             continue;
-        } 
+        }
 
-        if (i == 0 || i == 7 || i == 56 || i == 63) {
+        if (START_ROOK) {
             board[i] = ROOK << 1 | color;
             continue;
         }
 
-        if (i == 1 || i == 6 || i == 57 || i == 62) {
+        if (START_KNIGHT) {
             board[i] = KNIGHT << 1 | color;
             continue;
         }
 
-        if (i == 2 || i == 5 || i == 58 || i == 61) {
+        if (START_BISHOP) {
             board[i] = BISHOP << 1 | color;
             continue;
         }
 
-        if (i == 3 || i == 59) {
+        if (START_QUEEN) {
             board[i] = QUEEN << 1 | color;
             continue;
         }
 
-        if (i == 4 || i == 60) {
+        if (START_KING) {
             board[i] = KING << 1 | color;
             continue;
         }
 
         board[i] = 0;
     }
-} 
+}
 
 // find real positions from coordinates
 int coords_to_pos(int x, int y) {
@@ -124,6 +143,67 @@ int coords_to_pos(int x, int y) {
     }
 
     return -1;
+}
+
+int check_pawn(char* board, int src, int dest, char turn) {
+
+    int a = 0;
+    if (turn) {
+        a = src - 8;
+    } else {
+        a = src + 8;
+    }
+
+    if (a == dest) {
+        return 1;
+    }
+
+    return 0;
+}
+
+int check_knight(char *board, int src, int dest) {
+
+    return dest == src + 17
+        || dest == src + 15
+        || dest == src + 6
+        || dest == src + 10
+        || dest == src - 17
+        || dest == src - 15
+        || dest == src - 6
+        || dest == src - 10;
+}
+
+int check_rook(char *board, int src, int dest) {
+
+    if ((8 + max(dest, src) - min(dest, src)) % 8 == 0) {
+        return 1;
+    }
+
+    for (int i = 0; i <= (max(dest, src)+8)%8;i++) {
+        if (src == dest+i || src == dest-i) {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+int can_move(char* board, int src, int dest, char piece, char turn) {
+
+
+    char p = piece >> 1;
+
+    switch(p) {
+    case(PAWN):
+        return check_pawn(board, src, dest, turn);
+    case(ROOK):
+        return check_rook(board, src, dest);
+    case(KNIGHT):
+        return check_knight(board, src, dest);
+    default:
+        return 1;
+    }
+
 }
 
 int move(char* board, char* dest, int turn) {
@@ -158,7 +238,7 @@ int move(char* board, char* dest, int turn) {
     // check if missing locations
     if (from_x == -1 || from_y == -1 || to_x == -1 || to_y == -1) {
         return 0;
-    } 
+    }
 
     // check if same src as dst
     if (from_x == to_x && from_y == to_y) {
@@ -178,19 +258,28 @@ int move(char* board, char* dest, int turn) {
         printf("not your turn to move: %c\n", piece_to_char(piece));
         return 0;
     }
-    
-    // check if destination is not a of the same color
+
     int topos = coords_to_pos(to_x, to_y);
+    // check that position is really on the board
+    if (topos == -1) {
+        printf("invalid destination: %d %d\n", to_x, to_y);
+        return 0;
+    }
+    // check if destination is not a of the same color
     if (board[topos] != 0 && ((board[topos] << 7 & turn << 7) == turn << 7)) {
         printf("cant move there: %c\n", piece_to_char(piece));
         return 0;
     }
 
-    // XXX skipping a ton of checks here
-    board[pos] = 0;
-    board[topos] = piece;
+    // if this piece is allowed to move, move it
+    if (can_move(board, pos, topos, piece, turn)) {
+        board[pos] = 0;
+        board[topos] = piece;
+    } else {
+        printf("piece cant move there: %c\n", piece_to_char(piece));
+        return 0;
+    }
 
-    // really move the piece
 
     printf("moving: %c %c%c to %c%c\n", piece_to_char(piece), from_x + '0', from_y + '0', to_x + '0', to_y + '0');
 }
@@ -200,50 +289,87 @@ int main() {
     char board[8][8] = {};
 
     init_board(*board);
-    print_board(*board);
-
     printf("\n\n-----------------------------\n\n");
+    print_board(*board);
+    printf("\n\n");
+
 
 #ifdef DEBUG
     char *inputs[] = {
-        "1111", // test same src and dst
-        "1012", // test valid move
-        "1113", // test move when not my turn
-        "0604", // test my turns move
-        "1211", // test moving on my own piece
+        "1111 fail white", // test same src and dst
+        "0605 ok   black", // test my turns move
+        "1211 fail white", // test moving on my own piece
+        "1299 fail white", // test moving to an invalid location
+        "0102 ok   white", // test moving a pawn
+        "0504 ok   black", // test another pawn
+        "1021 fail white", // test knight
+        "1023 fail white", // test knight
+        "1022 ok   white", // test knight
+        "1705 ok   black", // test knight
+        "0010 ok   white", // test rook
+        "1014 ok   white", // test rook
+        "1464 ok   white", // test rook
+        "6453 fail white", // test rook
+        "6454 ok white", // test rook
+        "5455 ok white", // test rook
+        "5515 ok white", // test rook
         ""
     };
     int i  = 0;
+    int failed = -1;
 #endif
 
     char turn = 0;
 
     while(1) {
+#ifdef DEBUG
+        char *line = inputs[i];
+
+        if (strstr(inputs[i], "white")) {
+            turn = 0;
+        } else {
+            turn = 1;
+        }
+#endif
         if (turn) {
             printf("make a move (black): ");
         } else {
             printf("make a move (white): ");
         }
-#ifndef DEBUG
-        char *line = readline("");
-#else
-        char *line = inputs[i];
+#ifdef DEBUG
+
         printf("%s\n", line);
-        if (strlen(line) == 0) {
-            break;
-        }
-        i++;
-#endif
+        //getc(stdin);
         if (!move(*board, line, turn)) {
-            //printf("invalid move %s\n", line);
+            failed = 1;
         } else {
-            turn = turn ^ 1;
+            failed = 0;
+#else
+        char *line = readline("");
+        if (!move(*board, line, turn)) {
+        } else {
+            turn = turn ^ 1; // swap turn
+#endif
             printf("\n\n");
             print_board(*board);
             printf("\n\n");
         }
 #ifndef DEBUG
         free(line);
+#else
+        if (strstr(inputs[i], "fail") != NULL && !failed) {
+            printf("\n\nFAIL: %s should have failed\n", inputs[i]);
+            return 1;
+        }
+        if (strstr(inputs[i], "ok") != NULL && failed) {
+            printf("\n\nFAIL: %s should have been ok\n", inputs[i]);
+            return 1;
+        }
+        i++;
+        if (strlen(inputs[i]) == 0) {
+            printf("\n\nSUCCESS: all the moves were as expected!\n");
+            return 0;
+        }
 #endif
     }
 
